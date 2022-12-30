970acf3bd5f0ae35bca884e4a2398c3240d74848
unknown <bhotnikhil25@gmail.com>
2022-12-30 12:18:18 +0530

    [Nikhil]-Length Calculation

---
+++ UC01LengthCalculation/Program.cs
@@ -0,0 +1,13 @@
+﻿using LineComparisionProblem;
+
+namespace UC01LengthCalculation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program ");
+            LenghtCalculation.lenghtCalculation();
+        }
+    }
+}
