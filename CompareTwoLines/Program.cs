dc231de9134d50e3ccab54bebc04cae0fbad4820
unknown <bhotnikhil25@gmail.com>
2022-12-30 12:13:39 +0530

    [Nikhil]-Comapare Two Line

---
+++ CompareTwoLines/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparisionProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program ");
+            CompareTwoLines.compareTwoLines();
+        }
+    }
+}
